6cda16aac4929dc0629fb0dda526ff16a3f3a584
AlexeyEsipov <a_esipov@list.ru>
2022-05-31 16:44:49 +0300

    1. Техническое задание - проект "Работа мечты" [#504836]

---
+++ src/test/java/ru/job4j/AppTest.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
