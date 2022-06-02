21bc58a718f535ebdf10ea4fbbcb3f2812a5330b
AlexeyEsipov <a_esipov@list.ru>
2022-06-02 20:35:16 +0300

    4. PostController.updatePost. Редактирование вакансии. [#504850]

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -31,4 +31,12 @@ public class PostStore {
         post.setId(id.incrementAndGet());
         posts.put(post.getId(), post);
     }
+
+    public Post findById(int id) {
+        return posts.get(id);
+    }
+
+    public void update(Post post) {
+        posts.replace(post.getId(), post);
+    }
 }
